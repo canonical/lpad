c5cd4a0ed1a1b9e3f7f47a8fd7b9976bf18c2e05
Jani Monoses <jani@ubuntu.com>
2011-10-06 00:42:34 +0300

    Add Archive entry support

---
+++ archive.go
@@ -0,0 +1,52 @@
+package lpad
+
+import (
+	"os"
+)
+
+type Archive struct {
+	*Value
+}
+
+// Name of this archive
+func (a Archive) Name() string {
+	return a.StringField("name")
+}
+
+// User friendly name of this archive
+func (a Archive) DisplayName() string {
+	return a.StringField("displayname")
+}
+
+// Name of this archive
+func (a Archive) Description() string {
+	return a.StringField("description")
+}
+
+// The distribution that uses this archive
+func (a Archive) Distribution() Distro {
+	v, _ := a.Link("distribution_link").Get(nil)
+	return Distro{v}
+}
+
+// URL of this archive
+func (a Archive) SelfLink() string {
+	return a.StringField("self_link")
+}
+
+// WebPage returns the URL for accessing this archive in a browser.
+func (a Archive) WebPage() string {
+	return a.StringField("web_link")
+}
+
+type ArchiveList struct {
+	*Value
+}
+
+//For iterates over a list of archives and calls a function on each
+func (list ArchiveList) For(fn func(a Archive) os.Error) os.Error {
+	return list.Value.For(func(v *Value) os.Error {
+		fn(Archive{v})
+		return nil
+	})
+}
---
+++ archive_test.go
@@ -0,0 +1,22 @@
+package lpad_test
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/lpad"
+)
+
+func (s *ModelS) TestArchive(c *C) {
+	m := M{
+		"name":        "thename",
+		"displayname": "The Name",
+		"description": "The Description",
+		"self_link":   "http://apipage",
+		"web_link":    "http://page",
+	}
+	archive := lpad.Archive{lpad.NewValue(nil, "", "", m)}
+	c.Assert(archive.Name(), Equals, "thename")
+	c.Assert(archive.DisplayName(), Equals, "The Name")
+	c.Assert(archive.Description(), Equals, "The Description")
+	c.Assert(archive.SelfLink(), Equals, "http://apipage")
+	c.Assert(archive.WebPage(), Equals, "http://page")
+}
